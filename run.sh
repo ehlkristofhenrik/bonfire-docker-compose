67d804afb501a922376219ae3c97c680fe9161b0
ehlkristofhenrik <39805780+ehlkristofhenrik@users.noreply.github.com>
2024-12-13 09:32:32 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 wget "https://huggingface.co/Mozilla/Llama-3.2-1B-Instruct-llamafile/resolve/main/Llama-3.2-1B-Instruct.Q6_K.llamafile"
+chmod +x Llama-3.2-1B-Instruct.Q6_K.llamafile
 
 # sudo docker build -t d-bonfire-mock-llm -f mock.Dockerfile .
 sudo docker build -t d-bonfire-server -f server.Dockerfile .
 sudo docker build -t d-bonfire-shell -f shell.Dockerfile .
+
+./Llama-3.2-1B-Instruct.Q6_K.llamafile --server --system_prompt sys.txt
 
 sudo docker compose up -d
 
